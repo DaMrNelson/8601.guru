affd4a51c1ea5d1a118907c74422af573693c385
Nelson Schramm <nelson@nschramm.com>
2025-05-02 01:34:00 -0700

    Google analytics (all consent denied)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: 'G-F9PP5QPV9K',
+        consentDefaults: {
+          analytics_storage: "denied",
+          ad_storage: "denied",
+        },
+      },
+    }),
+  ],
   base: "/",
 })
