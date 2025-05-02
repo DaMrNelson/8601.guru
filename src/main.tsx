d8ac6dd2a074ce80e466aed81e10c391cfc80a11
Nelson Schramm <nelson@nschramm.com>
2025-05-02 16:04:14 -0700

    Custom page view tracker

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 import App from "./App.tsx";
+
+/** ANONYMOUS page view counter. Records nothing except the time the page was viewed.
+ *  No IP, cookies, etc are tracked EVER. */
+const ANONYMOUS_PAGE_VIEW_COUNTER = "https://bp2s45dvpfxxy5vex74td2vm6i0sdpuw.lambda-url.us-east-1.on.aws/";
+axios.post(ANONYMOUS_PAGE_VIEW_COUNTER);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
